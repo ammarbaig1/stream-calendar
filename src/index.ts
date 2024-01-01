d43320df3a6cc52829159b3a70102367ff598550
ammarbaig1 <128991949+ammarbaig1@users.noreply.github.com>
2024-01-01 17:11:48 +0500

    calendar

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,70 @@
-import { greetUser } from '$utils/greet';
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import listPlugin from '@fullcalendar/list';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import multiMonthPlugin from '@fullcalendar/multimonth';
+
+import type { Event } from './types';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  const calendarElement = document.querySelector<HTMLDivElement>('[data-element="calendar"]');
+  if (!calendarElement) return;
+
+  const events = getEvents();
+  console.log({ events });
+
+  const calendar = new Calendar(calendarElement, {
+    plugins: [dayGridPlugin, timeGridPlugin, listPlugin, listPlugin, multiMonthPlugin],
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'title multiMonthYear prev,next',
+      center: '',
+      right: '',
+    },
+
+    events: [
+      {
+        title: 'finest',
+        start: '2024-01-10',
+        end: '2024-01-11',
+        desc: 'Haaloo',
+      },
+    ],
+
+    eventClick(data) {
+      const dialogBoxElements = document.getElementsByClassName('dailog_box');
+      if (dialogBoxElements.length > 0) {
+        const dialogBox = dialogBoxElements[0];
+        dialogBox.classList.remove('none');
+      }
+
+      // const dialogBox = document.querySelector('.dialog_box');
+      // console.log(dialogBox);
+      // const title = document.getElementById('dialog_title');
+      // if (title) title.innerText = data.event.title;
+      // const display = dialogBox;
+      // dialogBox?.classList.remove('hidden');
+      // console.log('display: ', display);
+      // if (display === 'none') {
+      //   if (dialogBox) dialogBox.style.display = 'block';
+      // }
+      // alert(`User clicked the event ${data.event.title}`);
+    },
+  });
+
+  calendar.render();
 });
+
+const getEvents = (): Event[] => {
+  const scripts = document.querySelectorAll<HTMLScriptElement>('[data-element="event-data"]');
+  const events = [...scripts].map((script) => {
+    const event: Event = JSON.parse(script.textContent!);
+    event.start = new Date(event.start);
+    event.end = new Date(event.end);
+
+    return event;
+  });
+
+  return events;
+};
